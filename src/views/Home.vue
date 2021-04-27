ad991d5eb2b03ecb4909bbe06580946a71d7af65
Yoseph Parulian <yoseph.tdf@gmail.com>
2021-04-27 23:06:10 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderToko />
+    <HeroToko />
+    <WomenBanner />
+    <InstagramToko />
+    <PartnerLogo />
+    <FooterToko />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderToko from '@/components/HeaderToko.vue';
+import HeroToko from '@/components/HeroToko.vue';
+import WomenBanner from '@/components/WomenBanner.vue';
+import InstagramToko from '@/components/InstagramToko.vue';
+import PartnerLogo from '@/components/PartnerLogo.vue';
+import FooterToko from '@/components/FooterToko.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderToko,
+    HeroToko,
+    WomenBanner,
+    InstagramToko,
+    PartnerLogo,
+    FooterToko
+  }
+}
+</script>
